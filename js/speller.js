71254b97c8f82cffd256e06441247e25c8c52172
Bruno Kiafuka <brunokiafuka@live.com.pt>
2023-12-12 22:50:08 +0000

    Update speller.js

--- js/speller.js
+++ js/speller.js
@@ -1,30 +1,50 @@
 export default {
-	check,
-	lookup,
+  check,
+  lookup,
 };
 
 var elements;
 
 await loadPeriodicTable();
 
-
 // ****************************
 
 async function loadPeriodicTable() {
-	elements = await (await fetch("periodic-table.json")).json();
+  elements = await (await fetch("periodic-table.json")).json();
 }
 
 function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+  // TODO: determine if `inputWord` can be spelled
+  // with periodic table symbols; return array with
+  // them if so (empty array otherwise)
+  // because
+  if (inputWord.length > 0) {
+    for (let el of elements) {
+      const symbol = el.symbol.toLocaleLowerCase();
 
-	return [];
+      if (symbol.length <= inputWord.length) {
+        // did match symbol
+        if (inputWord.slice(0, symbol.length) == symbol) {
+          // still have chars left
+          if (inputWord.length > symbol.length) {
+            let res = check(inputWord.slice(symbol.length));
+            // match successfully
+            if (res?.length) {
+              return [symbol, ...res];
+            }
+          } else {
+            return [symbol];
+          }
+        }
+      }
+    }
+  }
+
+  //return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
-
-	return {};
+  // TODO: return the element entry based on specified
+  // symbol (case-insensitive)
+  return elements.find((v) => v.symbol.toLocaleLowerCase() == elementSymbol);
 }
